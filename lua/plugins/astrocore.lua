ec12c9e6bd4e4370449b91181ee4daa69803d4bb
Alexandr Bushuev <zxsnems@gmail.com>
2024-07-15 12:13:44 +0500

    autoformat, ack, set list

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -32,6 +32,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        listchars = "eol:¬,tab:!·,trail:·,extends:>,precedes:<,space:␣",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
